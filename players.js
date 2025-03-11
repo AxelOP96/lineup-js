e574d4e82349af0ddfed2f3b966b6f7f545adfc3
Axel <axeelleguero@gmail.com>
2025-03-11 20:19:04 -0300

    Css mas funcionalidad para que se carguen los datos dinamicamente

--- players.js
+++ players.js
@@ -1,28 +1,75 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const players = {
-        goalkeeper: ["Leandro Brey", "Agustin Marchesin", "Javier García", "Sergio Romero"],
-        defenders: ["Ayrton Costa", "Cristian Lema", "Marcelo Saracchi", "Nicolás Figal", "Marcos Rojo", "Luis Advíncula", "Frank Fabra", "Lautaro Blanco", "Juan Barinaga", "Lautaro Di Lollo", "Lucas Blondel", "Mateo Mendia", "Rodrigo Battaglia"],
-        midfielders: ["Ander Herrera", "Esteban Rolon", "Agustín Martegani", "Juan Ramirez", "Ignacio Miramon", "Kevin Zenon", "Tomás Belmonte", "Milton Delgado", "Mauricio Benitez", "Jabes Saralegui", "Leandro Paredes"],
-        attackers: ["Carlos Palacios", "Exequiel Zeballos", "Milton Giménez", "Édinson Cavani", "Lucas Janson", "Miguel Merentiel", "Brian Aguirre"]
-    };
-
-
-
-        const createOptions = (options, selectElement) => {
-                options.forEach(player => {
-                    const option = document.createElement("option");
-                    option.value = player;
-                    option.textContent = player;
-                    selectElement.appendChild(option);
-                });
-            };
-
-            createOptions(players.goalkeeper, document.getElementById("goalkeeper"));
-            createOptions(players.defenders, document.getElementsByClassName("defenders"));
-            createOptions(players.midfielders, document.getElementByByClassName("midfielders"));
-            createOptions(players.attackers, document.getElementByByClassName("attackers"));
-        });
-/*const players = {
-    goalkeeper: ["Player 1", "Player 2"],
-    defenders: ["Player 3", "Player 4", "Player 5", "Player 6"]
-};*/
+const players = {
+    goalkeeper: ["Leandro Brey", "Agustin Marchesin", "Javier García", "Sergio Romero"],
+    defenders: ["Ayrton Costa", "Cristian Lema", "Marcelo Saracchi", "Nicolás Figal", "Marcos Rojo", "Luis Advíncula", "Frank Fabra", "Lautaro Blanco", "Juan Barinaga", "Lautaro Di Lollo", "Lucas Blondel", "Mateo Mendia", "Rodrigo Battaglia"],
+    midfielders: ["Ander Herrera", "Esteban Rolon", "Agustín Martegani", "Juan Ramirez", "Ignacio Miramon", "Kevin Zenon", "Tomás Belmonte", "Milton Delgado", "Mauricio Benitez", "Jabes Saralegui", "Leandro Paredes"],
+    attackers: ["Carlos Palacios", "Exequiel Zeballos", "Milton Giménez", "Édinson Cavani", "Lucas Janson", "Miguel Merentiel", "Brian Aguirre"]
+};
+const d = document;
+const $field = d.querySelector(".game");
+const $arquero = d.getElementById("goalkeeper"),
+$defenders = d.querySelectorAll(".defender"),
+$midfielders = d.querySelectorAll(".midfielder"),
+$attackers = d.querySelectorAll(".attacker");
+d.addEventListener("DOMContentLoaded", (e)=>{
+    
+    
+    players.goalkeeper.forEach( (player) =>{
+        const option = d.createElement("option");
+        option.innerHTML = `${player}`;
+        $arquero.appendChild(option);
+    });
+    $arquero.firstElementChild.setAttribute("selected", "")
+    players.defenders.forEach( (player) =>{
+    
+        $defenders.forEach((defender) =>{
+            const option = d.createElement("option");
+            //console.log(defender.previousElementSibling)
+            option.innerHTML = `${player}`;
+            //console.log(players.defenders.indexOf(`${player}`))
+            /* if((option.innerHTML === defender.previousElementSibling.innerHTML)){
+            
+            } */
+            
+            console.log(option.innerHTML)
+            console.log(defender)    
+            defender.appendChild(option);
+            defender.firstElementChild.setAttribute("selected", "")
+        })
+        
+    });
+    
+    players.midfielders.forEach( (player) =>{
+    
+        $midfielders.forEach((midfielder) =>{
+            const option = d.createElement("option");
+            option.innerHTML = `${player}`;
+            midfielder.appendChild(option);
+            midfielder.firstElementChild.setAttribute("selected", "")
+        })
+        
+    });
+    players.attackers.forEach( (player) =>{
+    
+        $attackers.forEach((attacker) =>{
+            const option = d.createElement("option");
+            option.innerHTML = `${player}`;
+            attacker.appendChild(option);
+            attacker.firstElementChild.setAttribute("selected", "")
+        })
+         
+    });
+    /* const $jugadoresFinales = d.querySelectorAll("option[selected]");
+    $jugadoresFinales.forEach((jugador)=>{
+        const actual = jugador.value;
+        console.log(jugador)
+    }) */
+});
+d.addEventListener("change", (e)=>{
+    const $jugadoresFinales = d.querySelectorAll("option[selected]");
+    //console.log($jugadoresFinales)
+    $jugadoresFinales.forEach((jugador) =>{
+        const $span = d.createElement("span");
+        $span.textContent = `${jugador.value}`;
+        $field.appendChild($span);
+    })
+})
